008b4a3c5351965afc8baf9805aa93662f84222f
rjakkinappali-ontic <rjakkinappali@ontic.co>
2022-03-14 17:55:55 +0530

    Initial commit

---
+++ src/main/java/com/example/deployOnHerokuJAR/DeployOnHerokuJarApplication.java
@@ -0,0 +1,13 @@
+package com.example.deployOnHerokuJAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployOnHerokuJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployOnHerokuJarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/deployOnHerokuJAR/DeployOnHerokuJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deployOnHerokuJAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployOnHerokuJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
